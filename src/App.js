6001bf6b223fa40a7a24ac5c02f541fc926f4ad4
Valentina-Olekhnovich <volekh@gmail.com>
2020-11-20 10:16:57 +0100

    Yeee, display book cover correctly

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import BookShelfChanger from './BookShelfChanger.js'
 import BookShelf from './BookShelf.js'
 
 const shelf1 = [
-  {id:1, BookShelfChangerId:1, title: 'To Kill a Mockingbird', authors: 'Harper Lee', cover: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'},
-  {id:2, BookShelfChangerId:1, title: 'Enders Game', authors: 'Orson Scott Card', cover: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'},
+  {id:1, BookShelfChangerId:1, title: 'To Kill a Mockingbird', authors: 'Harper Lee', cover: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'},
+  {id:2, BookShelfChangerId:1, title: 'Enders Game', authors: 'Orson Scott Card', cover: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'},
 
 ]
 const shelf2 = [
--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import BookShelfChanger from './BookShelfChanger.js'
 
 export default function Book(props) {
-    return(
-
-
-
+        return(
+      <li>{
+        <div className="book">
+          <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.cover}")` }}></div>
+            <BookShelfChanger></BookShelfChanger>
+          </div>
+          <div className="book-title">{props.book.title}</div>
+          <div className="book-authors">{props.book.authors}</div>
+        </div>
+        }
+      </li>
     )
 }

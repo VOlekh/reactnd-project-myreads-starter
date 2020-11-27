5ef9e2ea68fa7b0b89eb54d1dc3589347a94e53d
Valentina-Olekhnovich <volekh@gmail.com>
2020-11-27 18:57:20 +0100

    Add 3 shelfs api call

--- src/App.js
+++ src/App.js
@@ -4,23 +4,6 @@ import './App.css'
 import BookShelf from './BookShelf.js'
 import SearchBooks from './SearchBooks.js'
 
-const shelf1 = [
-  {id:1, BookShelfChangerId:1, title: 'To Kill a Mockingbird', authors: 'Harper Lee', cover: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'},
-  {id:2, BookShelfChangerId:1, title: 'Enders Game', authors: 'Orson Scott Card', cover: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'},
-
-]
-const shelf2 = [
-  {id:1, BookShelfChangerId:2, title: '1776', authors: 'David McCullough', cover: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api'},
-  {id:2, BookShelfChangerId:2, title: 'Harry Potter and the Sorcerers Stone', authors: 'J.K. Rowling', cover: 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api'},
-
-]
-const shelf3 = [
-  {id:1, BookShelfChangerId:3, title: 'The Hobbit', authors: 'J.R.R. Tolkien', cover: 'http://books.http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api'},
-  {id:2, BookShelfChangerId:3, title: 'Oh, the Places Youll Go!', authors: 'Seuss', cover: 'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api'},
-  {id:2, BookShelfChangerId:3, title: 'The Adventures of Tom Sawyer', authors: 'Mark Twain', cover: 'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api'},
-
-]
-
 const currently_reading_shelf = {shelfTitle: 'Currently Reading', id: 'currentlyReading'};
 const want_to_read_shelf = {shelfTitle: 'Want To Read', id: 'wantToRead'};
 const read_shelf = {shelfTitle:'Read',   id: 'read'};
@@ -48,6 +31,8 @@ class BooksApp extends React.Component {
   render() {
     console.log ("BooksApi outcome", this.state.books)
     const books_on_shelf = this.state.books.filter (book =>book.shelf === 'currentlyReading' )
+    const books_on_shelf2 = this.state.books.filter (book =>book.shelf === 'wantToRead' )
+    const books_on_shelf3 = this.state.books.filter (book =>book.shelf === 'read' )
     console.log ("Filtered books on shelf", books_on_shelf)
 
 
@@ -63,8 +48,8 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
                 <BookShelf books={books_on_shelf} shelf={currently_reading_shelf}></BookShelf>
-                <BookShelf books={shelf2} shelf={want_to_read_shelf}></BookShelf>
-                <BookShelf books={shelf3} shelf={read_shelf}></BookShelf>
+                <BookShelf books={books_on_shelf2} shelf={want_to_read_shelf}></BookShelf>
+                <BookShelf books={books_on_shelf3} shelf={read_shelf}></BookShelf>
               </div>
             </div>
             <div className="open-search">

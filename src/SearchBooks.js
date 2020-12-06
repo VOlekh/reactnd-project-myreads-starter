d70e3411187ac4d607694d831d4d1e097f95850e
Valentina-Olekhnovich <volekh@gmail.com>
2020-12-06 23:12:49 +0100

    Update SearchBooks

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import BookShelf from './BookShelf'
+import Book from './Book.js'
 
 
 
@@ -11,19 +13,35 @@ class SearchBooks extends Component {
 // updateQuery() then calls setState(), merging in the new state to update the component's internal state.  
 updateQuery = (query) => {
   this.setState(
-    () =>({guery: query.trim()})
-  
+    () =>({
+      guery: query.trim()
+    })
   )
 }
 
+clearQuery = () => {
+  this.updateQuery('')
+  
+}
+
   render () {
+    const {query, books} = this.state
+
+    const showingBooks = query === ''
+      ? books
+      : books.filter((b) => (
+          b.title.toLowerCase().includes(query.toLowerCase())||
+          b.authors.toLowerCase().includes(query.toLowerCase())
+
+      ))
+
     return(
       <div className="search-books">
       
       <div className="search-books-bar">
         <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
         <div className="search-books-input-wrapper">
-        {JSON.stringify(this.state)}
+        {/* {JSON.stringify(this.state)} */}
           {/*
             NOTES: The search from BooksAPI is limited to a particular set of search terms.
             You can find these search terms here:
@@ -37,7 +55,7 @@ updateQuery = (query) => {
               className = 'search-books-input' 
               type="text" 
               placeholder="Search by title or author"
-              value = {this.state.query}
+              value = {query}
               // The onChange event listener invokes the updateQuery() function.
               onChange ={(event) => this.updateQuery(event.target.value)}
           />
@@ -45,10 +63,20 @@ updateQuery = (query) => {
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+        {/* <BookShelf books={books} title='Search' onUpdateBook={onUpdateBook}/> */}
+          {showingBooks.map((book) =>  (
+            <li key={book.id}>
+              <Book
+                book={book}
+              />
+            </li>
+            )
+          )}
+        </ol>
       </div>
       </div>
-    )   
+)
   } 
 }
 export default SearchBooks

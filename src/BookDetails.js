74e5f989145841b572d6cb9f15490ef9a967fa1e
Valentina-Olekhnovich <volekh@gmail.com>
2020-12-18 23:36:25 +0100

    Add html and css for BookDetails

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -10,18 +10,43 @@ export default function BookDetails (props) {
             return(<p>Loading...</p>);
         }
         return(
+            
             <div className="book-details">
-                <Link to='/'><button className="close-search" >Close</button></Link>
-                <div className="book-detail-top">
-                    {/* <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.imageLinks ? props.book.imageLinks.thumbnail : "#"}")` }}></div> */}
+                <div className='list-books-title'>
+                    <h1>MyReads</h1>
                 </div>
-                <div className="book-title">{props.book.title}</div>
-                <div className="book-authors">{props.book.authors}</div>
-                <div className="book-averageRating">{props.book.averageRating}</div>
-                <div className="book-categories">{props.book.categories}</div>
-                <div className="book-publisher">{props.book.publisher}</div>
-                <div className="book-page-count">{props.book.pageCount}</div>
-                <div className="book-description">{props.book.description}</div> 
+                <div className="list-books-content">
+                    <div className="bookshelf">
+                        <Link to='/'><button className="close-search" >Close</button></Link>
+                            <h2 className="bookshelf-title">Book details</h2>
+                        
+                        <div className="book-detail-top"></div>
+                        <div className= "card-wrapper">
+                            <div className="card">
+
+                                <div className = "book-img-wrapper">
+                                    <div className="book-cover" style={{ width: 256, height: 286, backgroundSize: 'auto', backgroundRepeat: 'no-repeat', backgroundImage: `url("${props.book.imageLinks ? props.book.imageLinks.thumbnail : "#"}")` }}></div>
+                                </div>
+
+                                <div className = "book-info-wrapper">
+                                    <h2 className="book-details-title">{props.book.title}</h2>
+                                    <h5 className="book-details-authors">Authors: {props.book.authors}</h5>
+                                    <ul>
+                                        <li className="book-averageRating">Average Rating: <span>{props.book.averageRating}</span></li>
+                                        <li className="book-categories"> Categories: <span>{props.book.categories}</span></li>
+                                        <li className="book-publisher">Publisher: <span>{props.book.publisher}</span></li>
+                                        <li className="book-page-count"> Number of pages: <span>{props.book.pageCount}</span></li>
+                                    </ul>
+                                </div>
+                    
+                                <div className = "book-description-wrapper">
+                                    <p className="book-description">{props.book.description}</p>
+                                </div>  
+
+                            </div>
+                        </div>
+                    </div>
+                </div>    
             </div>  
         )
 }
